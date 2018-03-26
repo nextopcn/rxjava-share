674a647bd1951f122c335ea57a5c865b9aee9637
xwei <xwei_01@hotmail.com>
2018-03-26 17:12:21 +0800

    u

--- src/main/java/cn/nextop/rxjava/share/practices/Practice5.java
+++ src/main/java/cn/nextop/rxjava/share/practices/Practice5.java
@@ -16,98 +16,98 @@
 
 package cn.nextop.rxjava.share.practices;
 
-import io.reactivex.Maybe;
-import io.reactivex.Observable;
-import io.reactivex.Single;
-
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Predicate;
+
+import cn.nextop.rxjava.share.util.Tuples;
+import io.reactivex.Maybe;
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.Single;
 
 /**
  * @author Baoyi Chen
  */
 public class Practice5 {
 
-    /*
-     * example:
-     * param: Observable["a","b","c"]
-     * return: Single[3]
-     */
-    public Single<Long> count(Observable<String> source) {
-        throw new UnsupportedOperationException("implementation");
-    }
+	/*
+	 * example: param: Observable["a","b","c"] return: Single[3]
+	 */
+	public Single<Long> count(Observable<String> source) {
+		return source.reduce(0L, (a, b) -> a + 1);
+	}
 
-    /*
-     * example:
-     * param: Observable[["a", "b", "c"], ["b", "c", "d"]]
-     * return: Observable["a", "b", "c","b", "c", "d"]
-     */
-    public Observable<String> convert(Observable<List<String>> source) {
-        throw new UnsupportedOperationException("implementation");
-    }
+	/*
+	 * example: param: Observable[["a", "b", "c"], ["b", "c", "d"]] return:
+	 * Observable["a", "b", "c","b", "c", "d"]
+	 */
+	public Observable<String> convert(Observable<List<String>> source) {
+		return source.concatMap(e -> Observable.fromIterable(e));
+	}
 
-    /*
-     * example:
-     * param: Observable["a", "a", "b", "b", "c"]
-     * return: Observable["a", "b", "c"]
-     */
-    public Observable<String> distinct(Observable<String> source) {
-        throw new UnsupportedOperationException("implementation");
-    }
+	/*
+	 * example: param: Observable["a", "a", "b", "b", "c"] return: Observable["a",
+	 * "b", "c"]
+	 */
+	public Observable<String> distinct(Observable<String> source) {
+		 return source.groupBy(e -> e).map(e -> e.getKey());
+	}
 
-    /*
-     * example:
-     * param: Observable[1, 2, 3, 4, 5] , conditon = x > 2 and x < 5
-     * return: Observable[3, 4]
-     */
-    public Observable<Integer> filter(Observable<Integer> source, Predicate<Integer> conditon) {
-        throw new UnsupportedOperationException("implementation");
-    }
+	/*
+	 * example: param: Observable[1, 2, 3, 4, 5] , conditon = x > 2 and x < 5
+	 * return: Observable[3, 4]
+	 */
+	public Observable<Integer> filter(Observable<Integer> source, Predicate<Integer> conditon) {
+		return source.concatMap(e -> {
+			if (conditon.test(e)) return Observable.just(e);
+			else return Observable.<Integer>empty();
+		});
+	}
 
-    /*
-     * example:
-     * param: Observable[1, 2, 3, 4, 5] , index = 2
-     * return: Maybe[3]
-     */
-    public Maybe<String> elementAt(Observable<String> source, int index) {
-        throw new UnsupportedOperationException("implementation");
-    }
+	/*
+	 * example: param: Observable[1, 2, 3, 4, 5] , index = 2 return: Maybe[3]
+	 */
+	public Maybe<String> elementAt(Observable<String> source, int index) {
+		return source.zipWith(Observable.range(0, Integer.MAX_VALUE), (a, b) -> Tuples.of(a, b)).filter(x -> x.getV2() == index).map(e -> e.getV1()).firstElement();
+	}
 
-    /*
-     * example:
-     * param: Observable["a", "b"] , count = 2
-     * return: Observable["a", "b", "a", "b"]
-     */
-    public Observable<String> repeat(Observable<String> source, int count) {
-        throw new UnsupportedOperationException("implementation");
-    }
+	/*
+	 * example: param: Observable["a", "b"] , count = 2 return: Observable["a", "b",
+	 * "a", "b"]
+	 */
+	public Observable<String> repeat(Observable<String> source, int count) {
+		return Observable.range(0, count).concatMap(e -> source);
+	}
 
-    /*
-     * example:
-     * param: Observable["a"], Observable["b"]
-     * return: Observable["a", "b"]
-     */
-    public Observable<String> concat(List<Observable<String>> source) {
-        throw new UnsupportedOperationException("implementation");
-    }
+	/*
+	 * example: param: Observable["a"], Observable["b"] return: Observable["a", "b"]
+	 */
+	 private void concat(List<Observable<String>> source, ObservableEmitter<String> emitter) {
+		 if (source.isEmpty()) {
+	            emitter.onComplete();
+	        } else {
+	            source.get(0).subscribe(e -> {
+	                emitter.onNext(e);
+	            }, e -> emitter.onError(e), () -> {
+	                concat(source.subList(1, source.size()), emitter);
+	            });
+	        }
+	}
 
-    /*
-     * example:
-     * param: Observable["a"], Observable["b"]
-     * return: Observable["a", "b"]
-     */
-    public Observable<String> merge(List<Observable<String>> source) {
-        throw new UnsupportedOperationException("implementation");
-    }
+	/*
+	 * example: param: Observable["a"], Observable["b"] return: Observable["a", "b"]
+	 */
+	public Observable<String> merge(List<Observable<String>> source) {
+		return Observable.fromIterable(source).concatMap(s -> s);
+	}
 
-    /*
-     * example:
-     * param: Observable["a", "b", "c"], 1, SECONDS
-     * return: Observable["a", "b", "c"], 每个元素都延迟1秒
-     */
-    public Observable<String> delayAll(Observable<String> source, long delay, TimeUnit unit) {
-        throw new UnsupportedOperationException("implementation");
-    }
+	/*
+	 * example: param: Observable["a", "b", "c"], 1, SECONDS return: Observable["a",
+	 * "b", "c"], 每个元素都延迟1秒
+	 */
+	public Observable<String> delayAll(Observable<String> source, long delay, TimeUnit unit) {
+		return source.flatMap(e -> Observable.just(e).delay(delay, unit));
+	}
 
 }
